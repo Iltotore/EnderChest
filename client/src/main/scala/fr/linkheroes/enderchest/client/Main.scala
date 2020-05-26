091fc85d32ac697b1f3ac44fb3b4886a40cff39d
BuildTools <unconfigured@null.spigotmc.org>
2020-05-26 11:53:02 +0200

    Added messaging system

--- client/src/main/scala/fr/linkheroes/enderchest/client/Main.scala
+++ client/src/main/scala/fr/linkheroes/enderchest/client/Main.scala
@@ -1,8 +1,16 @@
-package fr.linkheroes.enderchest.client;
+package fr.linkheroes.enderchest.client
+
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives.{extractRequest, path}
+;
 
 object Main {
 
   def main(args: Array[String]): Unit = {
     println("Enderchest -> Ready to use !")
+  }
+
+  def connect (string: String, int: Int): Unit = {
+
   }
 }
